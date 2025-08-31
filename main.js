4de6c70031102e8a61ad4811d7a3d49d8264c89d
Aristeu Escobar B. da Silva <aristeuescobar@hotmail.com>
2025-08-31 06:38:58 -0300

    More Added

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 // main.js
+console.log("Ae Test Page Loaded");
+let counter = 0;
+document.querySelector('button').addEventListener('click', function() {
+    console.log('Button was clicked!');
+    alert('Button Clicked!');
+    let element = document.getElementById('content');
+    element.style.backgroundColor = element.style.backgroundColor === 'lightblue' ? 'lightgreen' : 'lightblue';
+    counter++;
+    element.innerHTML = `Content changed! (${counter})`;
+    console.log(`Button clicked ${counter} times`);
+});
