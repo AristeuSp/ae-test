c56fd19763d0ead0352eb8ce67e1f6740f794214
Aristeu Escobar B. da Silva <aristeuescobar@hotmail.com>
2025-09-01 20:29:48 -0300

    playing with Ract 0

---
+++ src/components/react/MyReactComponent.jsx
@@ -0,0 +1,10 @@
+// src/components/MyReactComponent.jsx
+import React from 'react';
+
+const MyReactComponent = () => {
+    return (
+        <p>Este texto é de um componente React!</p>
+    );
+};
+
+export default MyReactComponent;
